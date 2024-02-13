d9d3b2f486585ccfb94a462a0766de563350b7a9
Charlie Ward <59178341+Charlie-Ward@users.noreply.github.com>
2024-02-13 11:37:10 +0000

    Added reload command

--- src/main/java/info/charlieward/lousynetplayercountutils/LousyNetPlayerCountUtils.java
+++ src/main/java/info/charlieward/lousynetplayercountutils/LousyNetPlayerCountUtils.java
@@ -1,9 +1,12 @@
 package info.charlieward.lousynetplayercountutils;
 
+import info.charlieward.lousynetplayercountutils.commands.reloadCommand;
 import info.charlieward.lousynetplayercountutils.files.CustomConfig;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class LousyNetPlayerCountUtils extends JavaPlugin {
+
+    private static LousyNetPlayerCountUtils plugin;
 
     @Override
     public void onEnable() {
@@ -17,6 +20,9 @@ public final class LousyNetPlayerCountUtils extends JavaPlugin {
         CustomConfig.get().addDefault("Server Name", "");
         CustomConfig.get().options().copyDefaults(true);
         CustomConfig.save();
+
+        getCommand("playerCountReload").setExecutor(new reloadCommand());
+
     }
 
     @Override
---
+++ src/main/java/info/charlieward/lousynetplayercountutils/commands/reloadCommand.java
@@ -0,0 +1,30 @@
+package info.charlieward.lousynetplayercountutils.commands;
+
+import info.charlieward.lousynetplayercountutils.files.CustomConfig;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.entity.Player;
+
+public class reloadCommand implements CommandExecutor {
+    
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (sender instanceof Player) {
+            Player p = (Player) sender;
+            if (p.hasPermission("LousyNetPlayerCountUtils.admin")) {
+                CustomConfig.reload();
+                p.sendMessage(ChatColor.BLUE + "[LousyNet-PlayerCount-Utils]" + ChatColor.GREEN + "Request for config to be reloaded has been sent");
+            } else {
+                p.sendMessage(ChatColor.BLUE + "[LousyNet-PlayerCount-Utils" + ChatColor.RED + "You do not have the correct permissions to run this command");
+            }
+        } else if (sender instanceof ConsoleCommandSender) {
+            CustomConfig.reload();
+            System.out.println("[LousyNet-PlayerCount-Utils] Request for config to be reloaded has been sent");
+        }
+
+        return true;
+    }
+}
